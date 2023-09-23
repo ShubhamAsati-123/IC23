7e34dc94693ca6122834fe763ba6dd8918a6414f
Ashmit Jagtap <ashmitjagtap2209@gmail.com>
2023-09-23 23:16:22 +0530

    team page added

--- src/components/Infinityteam.jsx
+++ src/components/Infinityteam.jsx
@@ -1,9 +1,18 @@
 import InfinityNav from "./InfinityNav"
+import Card from "./InfinityCards";
+import { team } from "../assets/team";
 
 const Infinityteam = () => {
   return (
-    <div className="bg-black h-screen">
-      <InfinityNav />
+    <div className="bg-black h-screen" >
+      {/* <InfinityNav /> */}
+      <div className="grid grid-rows-6 grid-flow-col gap-1">
+      {team.map(member =>{
+        console.log(member);
+        return <Card name = {member.name.toUpperCase()} designation={member.designation.toUpperCase()} github={member.github} linkedin={member.linkedin} insta={member.insta}/>
+      })}
+      
+      </div>
     </div>
   )
 }
