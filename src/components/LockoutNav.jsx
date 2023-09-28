f27bab33239b90e7c2aa656f003d2ad7e210d766
Ashmit Jagtap <ashmitjagtap2209@gmail.com>
2023-09-28 18:22:08 +0530

    changes to lockout

--- src/components/LockoutNav.jsx
+++ src/components/LockoutNav.jsx
@@ -3,14 +3,14 @@ import { useState } from "react"
 const LockoutNav =()=>{
 
     
-    return(<div className="text-2xl text-white font-miso font-thin bg-transparent p-4 flex justify-center space-x-28 tracking-widest">
-    <a href="/lockout" className="hover:text-lockoutPink hover:underline hover:underline-offset-2" >
-    <div>HOME</div>
-    </a>
-    <a href="/infinity/bitlegion" className="hover:text-lockoutPink hover:underline hover:underline-offset-2">
+    return(<div className="text-2xl text-white font-miso  bg-transparent p-9 flex justify-center  sticky top-0 space-x-20 tracking-widest">
+    <a href="#about" className=" hover:underline hover:underline-offset-2" >
+    <div>ABOUT</div>
+    </a> 
+    <a href="/infinity/bitlegion" className=" hover:underline hover:underline-offset-2">
     <div>BIT LEGION</div>
     </a>
-    <a href="/infinity/team" className="hover:text-lockoutPink hover:underline hover:underline-offset-2">
+    <a href="/infinity/team" className="hover:underline hover:underline-offset-2">
     <div>TEAM</div>
     </a>
 </div>)
